d9b8c11f8e4e7dd24181c6ac1971f1d842d62eb7
Sebastian Vittersø <svit@equinor.com>
2024-03-04 20:56:45 +0100

    update packages

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 //https://stackoverflow.com/a/45082999/11192976
 
-const execSync = require('child_process').execSync
+import { execSync } from 'child_process'
 
 console.log('\nBUILDING\n')
 execSync('npm run build', { stdio: 'inherit' })
