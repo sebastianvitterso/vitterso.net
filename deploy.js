7efd9a1e42cfd371fb4b7bb612b9fe29f8c56fda
Sebastian Vittersø <svit@equinor.com>
2025-01-20 10:09:14 +0100

    Importer fra node:

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 //https://stackoverflow.com/a/45082999/11192976
 
-import { execSync } from 'child_process'
+import { execSync } from 'node:child_process'
 
 console.log('\nBUILDING\n')
 execSync('npm run build', { stdio: 'inherit' })
