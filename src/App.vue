9861e0895fe09e54f223b3e626bd7c8f3800e89a
Sebastian Vittersø <svit@equinor.com>
2025-01-20 10:08:27 +0100

    Legg til mail-lenke på sida

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="w-full h-full flex justify-center items-center">
     <div class="w-full max-w-screen-lg bg-white mx-auto px-2 sm:px-8">
-      <header class="flex flex-col justify-center text-center items-center select-none">
-        <h1 class="text-4xl xs:text-7xl lg:text-8xl font-header mt-32 mb-12">Sebastian Vittersø</h1>
-        <h2 class="text-3xl xs:text-5xl lg:text-6xl font-header mb-32">
+      <header
+        class="flex flex-col justify-center text-center items-center select-none py-32 gap-12"
+      >
+        <h1 class="text-4xl xs:text-7xl lg:text-8xl font-header">Sebastian Vittersø</h1>
+        <h2 class="text-3xl xs:text-5xl lg:text-6xl font-header">
           Fullstack-utvikler med datateknologiutdanning.
         </h2>
+
+        <a
+          href="mailto:sebastian.vitters@gmail.com"
+          target="_blank"
+          class="text-4xl text-black hover:text-blue-800 visited:text-black font-header underline-offset-8 decoration-1"
+          >Vil du sende meg en mail?</a
+        >
       </header>
 
       <Face />
--- src/components/Face.vue
+++ src/components/Face.vue
@@ -1,11 +1,5 @@
 <template>
-  <div
-    class="absolute inset-0 w-screen h-screen overflow-hidden"
-    :class="[enableFace ? 'cursor-none' : '']"
-    @dblclick="toggleFace"
-    @mousemove="onMouseMove"
-    @touchmove="onTouchMove"
-  >
+  <div class="absolute inset-0 w-screen h-screen overflow-hidden pointer-events-none">
     <div
       v-if="enableFace"
       id="face"
@@ -18,7 +12,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onBeforeUnmount, onMounted, ref, watch } from 'vue'
 import faceImage from '../assets/face.png'
 
 const enableFace = ref(true)
@@ -27,6 +21,7 @@ const y = ref(0)
 
 function toggleFace() {
   enableFace.value = !enableFace.value
+  document.body.style.cursor = enableFace.value ? 'none' : 'auto'
 }
 function onTouchMove(event: TouchEvent) {
   const xPos = event.touches[0].clientX
@@ -44,6 +39,21 @@ function moveFace(xPos: number, yPos: number) {
   x.value = xPos - faceWidth / 2
   y.value = yPos - faceHeight / 2
 }
+
+onMounted(() => {
+  document.addEventListener('dblclick', toggleFace)
+  document.addEventListener('mousemove', onMouseMove)
+  document.addEventListener('touchmove', onTouchMove)
+  document.body.style.cursor = 'none'
+})
+onBeforeUnmount(() => {
+  document.removeEventListener('dblclick', toggleFace)
+  document.removeEventListener('mousemove', onMouseMove)
+  document.removeEventListener('touchmove', onTouchMove)
+  document.body.style.cursor = 'auto'
+})
+
+watch(enableFace, (newValue) => {})
 </script>
 
 <style>
